c2bbcc29fcd1b98746ec582820e5a01cc561ba6a
Alumno <Alumno>
2015-12-02 12:33:38 +0100

    Filtrar articulos ok

---
+++ categorias.php
@@ -0,0 +1,28 @@
+<?php
+
+$categoria= $_GET['categoria'];  
+
+switch ($categoria) {
+    case 1:
+        $articulos[0] = array('idArticulo' => '1', 'nombreArticulo' => 'art 1', 'descripcionArticulo' => 'desc 1', 'categoriaArticulo' => 'opera', 'imagenArticulo' => 'img 1', 'precioArticulo' => 'prec 1');
+        $articulos[1] = array('idArticulo' => '2', 'nombreArticulo' => 'art 2', 'descripcionArticulo' => 'desc 1', 'categoriaArticulo' => 'opera', 'imagenArticulo' => 'img 1', 'precioArticulo' => 'prec 1');
+        break;
+    case 2:
+                $articulos[0] = array('idArticulo' => '1', 'nombreArticulo' => 'art 1', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'hotel', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
+        $articulos[1] = array('idArticulo' => '2', 'nombreArticulo' => 'art 2', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'hotel', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
+        break;
+    case 3:      
+        $articulos[0] = array('idArticulo' => '1', 'nombreArticulo' => 'art 1', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'restaurante', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
+        $articulos[1] = array('idArticulo' => '2', 'nombreArticulo' => 'art 2', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'restaurante', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
+        break;
+    case 4:
+        $articulos[0] = array('idArticulo' => '1', 'nombreArticulo' => 'art 1', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'actividad', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
+        $articulos[1] = array('idArticulo' => '2', 'nombreArticulo' => 'art 2', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'actividad', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
+        break;
+}
+
+        require_once('json/Services_JSON.php');
+        $oJson = new Services_JSON();
+        echo $oJson->encode($articulos);
+
+?>
---
+++ js/index.js
@@ -0,0 +1,21 @@
+//Anyade la funcion click a los tags a con la clase categoria.
+$(document).ready(function() { 
+    document.getElementsByClassName("categorias").click=pedirDatos;
+});
+
+
+function pedirDatos(numeroDeCategoria) {       
+
+    $.ajax({                  
+        type: 'GET',
+        data: {categoria: +numeroDeCategoria},
+            
+        dataType: 'json',
+        url: 'categorias.php',
+        success: function(jsondata){
+            $.each(jsondata,function(){
+                alert('Nombre: ' + this.nombreArticulo + ', Categoria: ' + this.categoriaArticulo);
+            });
+        }
+    });
+};

a00ff56ee46bbbbcb5a830738653f6f9c41d2693
pedrodelbarrio <pedrodelbarriomartinez@gmail.com>
2015-12-14 18:29:48 +0100

    version final primer sprint

--- www/php/categorias.php
+++ www/php/categorias.php
@@ -1,28 +1,10 @@
 <?php
+    $categorias[0]=array ("nombre" => "Entradas");
+    $categorias[1]=array ("nombre" => "Alojamiento");
+    $categorias[2]=array ("nombre" => "Restaurantes");
+    $categorias[3]=array ("nombre" => "Tours y Actividades");
 
-$categoria= $_GET['categoria'];  
-
-switch ($categoria) {
-    case 1:
-        $articulos[0] = array('idArticulo' => '1', 'nombreArticulo' => 'art 1', 'descripcionArticulo' => 'desc 1', 'categoriaArticulo' => 'opera', 'imagenArticulo' => 'img 1', 'precioArticulo' => 'prec 1');
-        $articulos[1] = array('idArticulo' => '2', 'nombreArticulo' => 'art 2', 'descripcionArticulo' => 'desc 1', 'categoriaArticulo' => 'opera', 'imagenArticulo' => 'img 1', 'precioArticulo' => 'prec 1');
-        break;
-    case 2:
-                $articulos[0] = array('idArticulo' => '1', 'nombreArticulo' => 'art 1', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'hotel', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
-        $articulos[1] = array('idArticulo' => '2', 'nombreArticulo' => 'art 2', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'hotel', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
-        break;
-    case 3:      
-        $articulos[0] = array('idArticulo' => '1', 'nombreArticulo' => 'art 1', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'restaurante', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
-        $articulos[1] = array('idArticulo' => '2', 'nombreArticulo' => 'art 2', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'restaurante', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
-        break;
-    case 4:
-        $articulos[0] = array('idArticulo' => '1', 'nombreArticulo' => 'art 1', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'actividad', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
-        $articulos[1] = array('idArticulo' => '2', 'nombreArticulo' => 'art 2', 'descripcionArticulo' => 'desc 2', 'categoriaArticulo' => 'actividad', 'imagenArticulo' => 'img 2', 'precioArticulo' => 'prec 2');
-        break;
-}
-
-        require_once('../lib/json/Services_JSON.php');
-        $oJson = new Services_JSON();
-        echo $oJson->encode($articulos);
-
+    require_once('../lib/json/Services_JSON.php');
+    $oJson = new Services_JSON();
+    echo $oJson->encode($categorias);
 ?>

db4ee8d32604e8b838de1d7ff734a7cc1192e7c8
Pedro Del Barrio <pedrodelbarriomartinez@gmail.com>
2016-01-07 13:20:22 +0100

    version acabada segundo sprint

--- www/js/carrito.js
+++ www/js/carrito.js
@@ -1,34 +1,46 @@
-function Articulo(nombre, descripcion, precio, unidades) {
+function Reserva(nombre, categoria, descripcion, fechaEntrada, fechaSalida, hora, precio, unidades, rutaImagen) {
     this.nombre = nombre;
+    this.categoria = categoria;
     this.descripcion = descripcion;
+    this.fechaEntrada = fechaEntrada;
+    this.fechaSalida = fechaSalida;
+    this.hora = hora;
     this.precio = precio;
     this.unidades = unidades;
+    this.rutaImagen = rutaImagen;
 }
 
 function Carrito() {
-    this.articulos = new Array();
+    this.reservas = new Array();
 }
 
-Carrito.prototype.ponerArticulo = function(articulo) {
-    repetido = false;
-    for (i = 0; i < this.articulos.length && !repetido; i++) {
-        if (this.articulos[i].nombre === articulo.nombre) {
-        this.articulos[i].unidades++;
-        alert("Articulo " + this.articulos[i].nombre + " anyadido al carrito");
-        repetido = true;
+Carrito.prototype.ponerReserva = function(reserva) {
+    repetida = false;
+    for (i = 0; i < this.reservas.length && !repetida; i++) {
+        if (this.reservas[i].nombre === reserva.nombre && this.reservas[i].fechaEntrada === reserva.fechaEntrada) {
+            if (reserva.fechaSalida === null) {
+                this.reservas[i].unidades = this.reservas[i].unidades + reserva.unidades;
+                repetida = true;
+            } else if (this.reservas[i].fechaSalida === reserva.fechaSalida) {
+                repetida = true;
+            }
         }
     }
-    if (!repetido) {
-        this.articulos.push(articulo);
-        alert("Nuevo articulo " + this.articulos[i].nombre + " anyadido al carrito");
+    if (!repetida) {
+        this.reservas.push(reserva);
     }
+    return repetida;
 }
 
-Carrito.prototype.verArticulos = function() {
+Carrito.prototype.eliminarReserva = function(indexReserva) {
+    this.reservas.splice(indexReserva, 1);
+}
+
+Carrito.prototype.verReservas = function() {
     precioTotal = 0;
-    for (i = 0; i < this.articulos.length; i++) {
-        precioTotal = precioTotal + this.articulos[i].precio*this.articulos[i].unidades;
-        alert("Articulo: " + this.articulos[i].nombre + "\nPrecio: " + this.articulos[i].precio + " €\nUnidades: " + this.articulos[i].unidades);
+    for (i = 0; i < this.reservas.length; i++) {
+        precioTotal = precioTotal + this.reservas[i].precio*this.reservas[i].unidades;
+        alert("Articulo: " + this.reservas[i].nombre + "\nPrecio: " + this.reservas[i].precio + " €\nUnidades: " + this.reservas[i].unidades);
     }
 
     alert("Total: " + precioTotal + " €");

6a4bde9feeebcbee289761b3aac083556226c3af
Pedro Del Barrio <pedrodelbarriomartinez@gmail.com>
2016-01-07 13:13:43 +0100

    version acabada segundo sprint

--- www/php/categorias.php
+++ www/php/categorias.php
@@ -1,10 +1,26 @@
 <?php
-    $categorias[0]=array ("nombre" => "Entradas");
-    $categorias[1]=array ("nombre" => "Alojamiento");
-    $categorias[2]=array ("nombre" => "Restaurantes");
-    $categorias[3]=array ("nombre" => "Tours y Actividades");
+    error_reporting(E_ALL ^ (E_NOTICE | E_WARNING | E_DEPRECATED));
 
-    require_once('../lib/json/Services_JSON.php');
-    $oJson = new Services_JSON();
-    echo $oJson->encode($categorias);
+    // Conectando, seleccionando la base de datos
+
+    $connection = mysql_connect('localhost', 'root', '')
+        or die('No se pudo conectar: ' . mysql_error());
+    mysql_select_db('tienda') or die('No se pudo seleccionar la base de datos');
+
+    // Realizar una consulta MySQL
+
+    $query = 'SELECT * FROM categorias';
+    $result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
+    while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+            $categorias[] = $line;
+    }
+
+    // Liberar resultados
+
+    mysql_free_result($result);
+
+    // Cerrar la conexión
+
+    mysql_close($connection);
+    echo json_encode($categorias);
 ?>

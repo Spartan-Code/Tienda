ade2f1f1c2dd836c06273ed42fb7a4a0cb2d71ae
alumno <alumno>
2016-02-17 11:43:12 +0100

    tienda ok

--- www/php/imprimir_facturas.php
+++ www/php/imprimir_facturas.php
@@ -30,7 +30,7 @@ $pdf->AddPage();
 $pdf->SetFont('Arial','',16);
 
 $pdf->Image('../img/opera_valencia_logo.png',10,10, -150);
-$pdf->Cell(85,20,'Desglose del pedido '.$numeroPedido, 0, 0, 'R');
+$pdf->Cell(90,20,'Desglose del pedido '.$numeroPedido, 0, 0, 'R');
 $pdf->Ln();
 
 $query = 'SELECT idArticulo, nombreArticulo, unidades, precio FROM lineaPedidos WHERE idPedido = "'.$numeroPedido.'"';
@@ -49,32 +49,45 @@ while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
 
 
     $header = array('ID Articulo', 'Nombre', 'Unidades', 'Precio');
-    // Cabecera
+
+    $widthCeldas=array(10, 50, 8, 8);
     $pdf->SetFillColor(169, 144, 90);
-    foreach($header as $col)
-        $pdf->Cell(45,12,$col,1, 0, 'C', True);
+
+    // Cabecera
+    $w = array(30, 100, 30, 30);
+    for($i=0;$i<count($header);$i++)
+        $pdf->Cell($w[$i],7,$header[$i],1,0,'C',true);
     $pdf->Ln();
 
-    // Datos
-    $widthCeldas=array(10, 50, 40, 80);
+    $preciototal = 0;
+    for($i=0;$i<count($datos);$i++){
+            $preciototal += $datos[$i]["precio"];
+    }
 
     foreach($datos as $row)
     {
         $i=0;
-        if($i%2==0)
+        $x=0;
+        
+        if($x%2==0)
         {
-            $pdf->SetFillColor(255, 255, 255);
+            $pdf->SetFillColor(0, 255, 255);
         }
         else
         {
            $pdf->SetFillColor(245, 240, 229); 
         }        
-        foreach($row as $col)
-        
-            $pdf->Cell(45,10,$col,1, 0, 'C', True);
-            $pdf->Ln();
+        foreach($row as $col){
+            $pdf->Cell($w[$i],10,$col,1, 0, 'C');
             $i++;
+        }
+            $pdf->Ln();
+        $x++;
     }
+
+
+
+    $pdf->Cell(180 ,20,'Precio Total: '.$preciototal, 0, 0, 'R');
 
 $pdf->Output('pedido_'.$numeroPedido.'.pdf','I');
 ?>

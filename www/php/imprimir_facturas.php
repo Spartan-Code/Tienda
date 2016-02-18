f124ca8e0cb8fe4d865b3adb463ab0458d7ced45
alumno <alumno>
2016-02-18 09:54:48 +0100

    ajshkdajhsdkjh

--- www/php/imprimir_facturas.php
+++ www/php/imprimir_facturas.php
@@ -89,7 +89,7 @@ while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
 
     $pdf->Cell(180 ,20,'Precio Total: '.$preciototal, 0, 0, 'R');
 
-$content = $pdf->Output('pedido_'.$numeroPedido.'.pdf','D');
+$content = $pdf->Output('F','./pedido_'.$numeroPedido.'.pdf');
 
 
 ?>
